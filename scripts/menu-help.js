5428e98feff6ec442c7a35641ec8e32381e291fa
Giorgio Alefe <giorgioa2002@gmail.com>
2021-05-23 15:10:50 -0300

    Create folder scripts and css

---
+++ scripts/menu-help.js
@@ -0,0 +1,25 @@
+const menuHelp = document.querySelector('.menu-help');
+const closeButton = document.querySelector('.close-menu');
+const openButton = document.querySelector('.main-button.menu.open');
+
+openButton.addEventListener('click', open)
+closeButton.addEventListener('click', close);
+
+document.addEventListener('keydown', closeByEsc)
+
+function open() {
+    menuHelp.classList.toggle('on');
+}
+
+function close() {
+    menuHelp.classList.toggle('on');
+}
+
+function closeByEsc(event) {
+    const isEsc = event.key == 'Escape';
+    console.log(event.key);
+
+    if (isEsc) {
+        menuHelp.classList.remove('on')
+    }
+}
