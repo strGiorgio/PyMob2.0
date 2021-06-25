59a9cb72c24c770ce25c68d43e5911a11db498da
Giorgio Alefe <giorgioa2002@gmail.com>
2021-06-25 17:56:16 -0300

    Create new script to show names and add points

---
+++ scripts/attributes-script.js
@@ -0,0 +1,8 @@
+const nameValue = document.querySelector('#name');
+const pointsValue = document.querySelector('#points');
+
+points = 10;
+pointsValue.innerHTML = points;
+
+nameValue.innerHTML = "Robert";
+
